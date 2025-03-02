779f2e0e073795eae3885242b8f1b39dafd32874
CSerratoDev <cserrato.dev@gmail.com>
2025-03-01 20:26:11 -0600

    Initial config for locations route

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,13 @@
-export class CreateLocationDto {}
+import { ArrayNotEmpty, IsArray, isArray, IsString, MaxLength } from "class-validator";
+
+export class CreateLocationDto extends Location{
+    @IsString()
+    @MaxLength(35)
+    locationName: string;
+    @IsString()
+    @MaxLength(160)
+    adress: string;
+    @IsArray()
+    @ArrayNotEmpty()
+    locationLatLng: number[]
+}
