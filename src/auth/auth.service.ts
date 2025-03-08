b597767fad997f9723824725e15df72836ba7760
CSerratoDev <cserrato.dev@gmail.com>
2025-03-08 17:23:20 -0600

    Added bcrypt for password encryption

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,22 @@ export class AuthService {
         private userRepository: Repository<User>
     ){}
     registerUser(createUserDto: CreateUserDto){
+        createUserDto.userPassword = bcrypt.hashSync(createUserDto.userPassword, 5)
        return this.userRepository.save(createUserDto)
+    }
+    async loginUser(createUserDto: CreateUserDto){
+        const user = await this.userRepository.findOne({
+            where: {
+                userEmail: createUserDto.userEmail
+            }
+        })
+        if (!user) {
+            throw new UnauthorizedException("User not found");
+        }
+        const match = await bcrypt.compare(createUserDto.userPassword, user.userPassword);
+        if (!match) {
+            throw new UnauthorizedException("You are not authorized");
+        }
+        return    
     }
 }
