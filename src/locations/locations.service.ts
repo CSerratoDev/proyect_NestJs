9c6676c76989d123dbfe236b717c35d1eedbca72
CSerratoDev <cserrato.dev@gmail.com>
2025-04-03 22:15:59 -0600

    fix: Return location on create

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -24,6 +24,7 @@ export class LocationsService {
       locationId: id,
     });
     if(!location) throw new NotFoundException()
+      return location;
   }
 
   async update(id: number, updateLocationDto: UpdateLocationDto) {
